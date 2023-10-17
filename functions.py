0ed563fbe294124396b9265f3695b90e23a2d2d6
Andréa Joly <andrea.joly1989@gmail.com>
2023-10-17 19:17:34 +0200

    wip

--- functions.py
+++ functions.py
@@ -2,23 +2,60 @@ from collections import defaultdict
 import subprocess
 import os
 import csv
+import pandas as pd
+import numpy as np
 
 
-def retrieve_data(host : str, user : str, pwd : str, port : int = 623) -> str:
-    try : 
-        command = f'ipmitool -I lanplus -H {host} -p {port} -U {user} -P {pwd} sdr elist full > temp.csv'
-        result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
-        return result.stdout
+class IPMIConnector:
+    def __init__(self, host : str, user : str, pwd : str, port : int = 623):
+        self.host = host
+        self.user = user 
+        self.password = pwd
+        self.port = port
+        self
     
-    except Exception as e :
-        return e
+    def retrieve_data(self) -> pd.DataFrame:
+        try : 
+            command = f"ipmitool -I lanplus -H {self.host} -p {self.port} -U {self.user} -P {self.pwd} sdr elist full > temp.csv"
+            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, text=True)
+            command = f"sed -i 'name | ID | status | group | metric' temp.csv"
+            self.df = pd.read_csv("temp.csv", delimiter="|")
+
+            return result.stdout
+        except Exception as e:
+            return e
+    
+    def sanitize_data(self):
+        df = pd.read_csv("temp.csv", delimiter="|")
+        df.columns = df.columns.str.strip()
+        df = df.replace({' ':''}, regex=True)
+        df['unit'] = df['metric'].str.extract(r'([^0-9.,]+)$')
+        df['metric'] = df['metric'].str.replace(r'[^\d,.]', '', regex=True)
+        df['metric'] = df['metric'].replace('', np.nan)
+        df = df.dropna(subset=['metric'])
 
 
-def process_data(data) -> dict :
-    with open('temp.csv') as csv_file : 
+
+def sanitize_data():
+
+
+
+
+    print(df.head(20))
+    print(df.columns)
+
+
+def process_data():
+    with open('temp.csv','w') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter='|')
-        for row in csv_reader: 
+        for row in csv_reader:
+            send_data(row)
+            for item in row:
+                item = item.replace(" ", "")
+                print(item)
             print(row)
 
 
-process_data(retrieve_data('192.168.1.66', 'root', 'calvin'))
+sanitize_data()
+
+#process_data(retrieve_data('192.168.1.66', 'root', 'calvin'))
--- test.py
+++ test.py
@@ -4,29 +4,27 @@ client = InfluxDBClient(host='localhost', port=8086)
 if 'ipmi_metrics' not in client.get_list_database():
     client.create_database('ipmi_metrics')
 
-client.switch_database('ipmi_metrics')
+def send_data(file_path : str) :
+    client = InfluxDBClient(host='localhost', port=8086)
+    client.switch_database('ipmi_metrics')
 
-query = 'SHOW MEASUREMENTS'
+    data = [
+        {
+            "measurement": "voltage", 
+            "fields": {
+                "value": 12.0
+            }
+        }
+    ]
+
+    client.write_points(data)
 
 query = 'SELECT * FROM voltage LIMIT 10' 
-
+query = 'SHOW MEASUREMENTS'
 result = client.query(query)
-
-data = [
-    {
-        "measurement": "voltage", 
-        "fields": {
-            "value": 12.0  
-        }
-    }
-]
-
-client.write_points(data)
 
 for measurement in result:
     for point in measurement:
         print(point)
-
-
 
 #print(client.get_list_database())

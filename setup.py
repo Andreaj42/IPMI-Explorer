4d9de93168bee888d26c5c6f057d5b7d7c14de31
Andreaj42 <andrea.joly1989@gmail.com>
2023-11-21 22:04:00 +0100

    update packages files

--- setup.py
+++ setup.py
@@ -1,12 +1,15 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='ipmi-explorer',
     version='1.0',
+    packages=find_packages(include=['lib', 'config']),
     install_requires=[
         'APScheduler==3.10.1',
         'mysql-connector-python==8.0.33',
-        'setuptools==58.1.0'
+        'setuptools==58.1.0',
+        'python-dotenv',
+        'pandas==2.1.3'
     ],
     description='Retrieve Data from server to Mariadb by IPMI protocol.',
 )

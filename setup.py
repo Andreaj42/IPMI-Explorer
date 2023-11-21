35217562b42cc058d0bca094b46d17474c396896
Andreaj42 <andrea.joly1989@gmail.com>
2023-11-21 22:26:34 +0100

    update setuptools

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     install_requires=[
         'APScheduler==3.10.1',
         'mysql-connector-python==8.0.33',
-        'setuptools==58.1.0',
+        'setuptools==65.5.1',
         'python-dotenv',
         'pandas==2.1.3'
     ],
